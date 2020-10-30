d43b024a9deaf7ca2424e5b88a8fa47b79f23067
Thomaz T. O. Toyama <thomazot@gmail.com>
2020-10-30 14:21:07 -0300

    feat: install styled-component and configure

---
+++ src/Theme.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+
+}
+
+export interface ITheme {
+  children: ReactNode
+}
+
+const Theme = ( { children }: ITheme) => {
+  return <ThemeProvider theme={theme}>{ children }</ThemeProvider>
+}
+
+export default Theme
